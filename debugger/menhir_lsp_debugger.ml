f502e90da01dee89e4256aebab47f4c036752687
dalps <dalpio23@gmail.com>
2026-04-11 00:51:33 +0200

    define a linkage plugin for debuggee programs

---
+++ debugger/menhir_lsp_debugger.ml
@@ -0,0 +1,28 @@
+open MenhirLib
+
+(** Barrel module containing all the information needed to start a debugging
+    session. Define it and feed it to [Linkage.provide] *)
+module type M = sig
+  type semantic_value
+
+  module Parser : sig
+    type token
+
+    module Tables : TableFormat.TABLES with type token = token
+
+    module MenhirInterpreter :
+      IncrementalEngine.EVERYTHING with type token = token
+
+    module Incremental : sig
+      val main : Lexing.position -> semantic_value MenhirInterpreter.checkpoint
+    end
+  end
+
+  module Lexer : sig
+    val token : Lexing.lexbuf -> Parser.token
+  end
+end
+
+type Linkage.plugin += Menhir_lsp_plugin of (module M)
+
+let provide = Linkage.provide
