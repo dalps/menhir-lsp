d77fb934f1e6571746ccfe892e0ccba564031f18
dalps <dalpio23@gmail.com>
2026-04-11 21:58:46 +0200

    set up dune sites

---
+++ debug/plugin_intf.ml
@@ -0,0 +1,24 @@
+open MenhirLib
+
+(** Barrel module containing all the information needed to start a debugging
+    session. Define it and feed it to [provide] *)
+module type M = sig
+  type semantic_value
+
+  module Parser : sig
+    type token
+
+    module Tables : TableFormat.TABLES with type token = token
+
+    module MenhirInterpreter :
+      IncrementalEngine.EVERYTHING with type token = token
+
+    module Incremental : sig
+      val main : Lexing.position -> semantic_value MenhirInterpreter.checkpoint
+    end
+  end
+
+  module Lexer : sig
+    val token : Lexing.lexbuf -> Parser.token
+  end
+end
