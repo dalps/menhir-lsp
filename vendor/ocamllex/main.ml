e52ba05fd678150e386cc4296e1e60a552941d52
dalps <dalpio23@gmail.com>
2026-07-18 00:03:29 +0200

    Write error messages for ocamllex grammar

--- vendor/ocamllex/main.ml
+++ vendor/ocamllex/main.ml
@@ -1,19 +1,25 @@
 let parse lexbuf =
   Lexer.init ();
   Hashtbl.reset Syntax.named_regexps;
-  try Ok (Parser.lexer_definition Lexer.main lexbuf) with
-  | Syntax.SyntaxError { v; p; _ } | Lexer.Lexical_error { v; p; _ } ->
+  try
+    Original_parser.lexer_definition Lexer.main lexbuf;
+    try Ok (Parser.lexer_definition Lexer.main lexbuf)
+    with Syntax.SyntaxError { v; p; _ } | Lexer.Lexical_error { v; p; _ } ->
       Error (v, p)
-  | _ ->
-      let range =
-        Range.make Lexing.(lexeme_start_p lexbuf, lexeme_end_p lexbuf)
-      in
-      Error
-        (Printf.sprintf "Syntax error near '%s'" @@ Lexing.lexeme lexbuf, range)
+  with Original_parser.Error state ->
+    let message = Original_parser_messages.message state in
+    let range =
+      Range.make Lexing.(lexeme_start_p lexbuf, lexeme_end_p lexbuf)
+    in
+    Error
+      ( Printf.sprintf "Syntax error near '%s':\n%s" (Lexing.lexeme lexbuf)
+          message,
+        range )
 
 let parse_file file =
   let inp = open_in file in
   let lexbuf = Lexing.from_channel inp in
+  Lexing.set_filename lexbuf file;
   let ast = parse lexbuf in
   close_in inp;
   ast
