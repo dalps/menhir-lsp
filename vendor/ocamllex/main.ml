9bba22d539ee21d752786ae1371072e7df1447a6
dalps <dalpio23@gmail.com>
2026-07-18 17:48:02 +0200

    ocamllex: adopt two-stage parsing
    The first pass deals with syntax errors whose messages are obtained from the states of the official parser (stripped of semantics).
    The second pass is executed only when first pass succeeds and produces our custom AST type.
    TODO: verify that the two parsers accept the same language.

--- vendor/ocamllex/main.ml
+++ vendor/ocamllex/main.ml
@@ -1,27 +1,40 @@
-let parse lexbuf =
+let parse get_lexbuf =
   Lexer.init ();
   Hashtbl.reset Syntax.named_regexps;
+  let lexbuf = get_lexbuf () in
   try
     Original_parser.lexer_definition Lexer.main lexbuf;
-    try Ok (Parser.lexer_definition Lexer.main lexbuf)
-    with Syntax.SyntaxError { v; p; _ } | Lexer.Lexical_error { v; p; _ } ->
+    let lexbuf = get_lexbuf () in
+    try Ok (Parser.lexer_definition Lexer.main lexbuf) with
+    | Parser.Error ->
+        Error
+          ( "Parser error",
+            Lexing.(lexbuf.lex_start_p, Lexing.lexeme_end_p lexbuf) )
+    | Syntax.SyntaxError { v; p; _ } | Lexer.Lexical_error { v; p; _ } ->
+        Error (v, p)
+  with
+  | Original_parser.Error state ->
+      let message = Original_parser_messages.message state in
+      let range =
+        Range.make Lexing.(lexeme_start_p lexbuf, lexeme_end_p lexbuf)
+      in
+      Error
+        ( Printf.sprintf "Syntax error near '%s':\n%s" (Lexing.lexeme lexbuf)
+            message,
+          range )
+  | Syntax.SyntaxError { v; p; _ } | Lexer.Lexical_error { v; p; _ } ->
       Error (v, p)
-  with Original_parser.Error state ->
-    let message = Original_parser_messages.message state in
-    let range =
-      Range.make Lexing.(lexeme_start_p lexbuf, lexeme_end_p lexbuf)
-    in
-    Error
-      ( Printf.sprintf "Syntax error near '%s':\n%s" (Lexing.lexeme lexbuf)
-          message,
-        range )
 
 let parse_file file =
   let inp = open_in file in
-  let lexbuf = Lexing.from_channel inp in
-  Lexing.set_filename lexbuf file;
-  let ast = parse lexbuf in
+  let get_lexbuf () =
+    seek_in inp 0;
+    let lexbuf = Lexing.from_channel inp in
+    Lexing.set_filename lexbuf file;
+    lexbuf
+  in
+  let ast = parse get_lexbuf in
   close_in inp;
   ast
 
-let parse_string s = parse (Lexing.from_string s)
+let parse_string s = parse (fun () -> Lexing.from_string s)
