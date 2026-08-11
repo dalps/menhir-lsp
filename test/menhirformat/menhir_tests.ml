79900f912ee49767ccd022fbff1a3103d84c33cb
Paul Robert <pauulrobert@gmail.com>
2026-08-11 11:00:35 +0200

    add module-parametrized test
    Taken from menhir's examples

--- test/menhirformat/menhir_tests.ml
+++ test/menhirformat/menhir_tests.ml
@@ -403,3 +403,53 @@ lax_actual:
       branches = located(branches)
       { locate' $loc @@ ParamAnonymous branches }
     |}]
+
+let%expect_test "Formatting of parser parametrized by a module" =
+  helper
+    {|
+(* Taken from https://github.com/LexiFi/menhir/blob/master/demos/calc-param/parser.mly *)
+%parameter<Semantics : sig
+  type number
+  val inject: int -> number
+  val ( + ): number -> number -> number
+  val ( - ): number -> number -> number
+  val ( * ): number -> number -> number
+  val ( / ): number -> number -> number
+  val ( ~-): number -> number
+end>
+
+(* The parser no longer returns an integer; instead, it returns an
+   abstract number. *)
+
+%start <Semantics.number> main
+
+(* Let us open the [Semantics] module, so as to make all of its
+   operations available in the semantic actions. *)
+
+%{
+
+  open Semantics
+
+%}
+
+%%
+
+main:
+| e = expr EOL
+    { e }
+
+expr:
+| i = INT
+    { inject i }
+| LPAREN e = expr RPAREN
+    { e }
+| e1 = expr PLUS e2 = expr
+    { e1 + e2 }
+| e1 = expr MINUS e2 = expr
+    { e1 - e2 }
+| e1 = expr TIMES e2 = expr
+    { e1 * e2 }
+| e1 = expr DIV e2 = expr
+    { e1 / e2 }
+| MINUS e = expr %prec UMINUS
+    { - e } |}; [%expect]
